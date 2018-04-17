3c5c048b2ff817fda4069994001e28058eebd240
Michael Sumner <mdsumner@gmail.com>
2018-04-17 14:56:06 +1000

    skip 32-bit Windows testing of Thredds

--- tests/testthat/test-thredds.R
+++ tests/testthat/test-thredds.R
@@ -2,6 +2,7 @@ context("test-thredds.R")
 u <- "http://coastwatch.pfeg.noaa.gov/erddap/griddap/erdQSwind3day"
 
 test_that("thredds works", {
+  skip_if(.Platform$OS.type == "windows" && .Platform$r_arch != "x64")
   expect_that(ncdf4::nc_open(u), is_a("ncdf4"))
   
   expect_that(raster::raster(u, ncdf = TRUE, varname = "x_wind"), is_a("BasicRaster"))
